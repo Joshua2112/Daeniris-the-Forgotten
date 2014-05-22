ea049c6a0fca14a36777fadb009e8ba246d11a61
Joshua2112 <Joshua.lynn.larson@gmail.com>
2014-05-22 16:26:17 -0600

    some more objects

---
+++ DaenirisTheForgotten/test/byui/CIT260/DaenirisTheForgotten/Control/ShoppingTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.CIT260.DaenirisTheForgotten.Control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Joshua
+ */
+public class ShoppingTest {
+    
+    public ShoppingTest() {
+    }
+
+    /**
+     * Test of purchaseItem method, of class Shopping.
+     */
+    @Test
+    public void testPurchaseItem() {
+        System.out.println("purchaseItem");
+        boolean itemAvailable = true;
+        int result;
+        int playerGold = 30;
+        int itemCost = 20;
+        int quantityOfItem = 0;
+        Shopping instance = new Shopping();
+        int expResult = 1;
+        result = instance.purchaseItem(itemAvailable, playerGold, itemCost, quantityOfItem);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    
+}
