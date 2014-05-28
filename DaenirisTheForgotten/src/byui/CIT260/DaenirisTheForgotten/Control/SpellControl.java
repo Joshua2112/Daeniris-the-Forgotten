c284df920e5da042a5b310feab6521f01610c539
Joshua2112 <Joshua.lynn.larson@gmail.com>
2014-05-28 16:00:23 -0600

    SpellControl Caps

--- DaenirisTheForgotten/src/byui/CIT260/DaenirisTheForgotten/Control/SpellControl.java
+++ DaenirisTheForgotten/src/byui/CIT260/DaenirisTheForgotten/Control/SpellControl.java
@@ -1,13 +1,15 @@
-/*********************************************************************
- * Daeniris The Forgotten (Role Playing Game)
- * Author: Nathan Wittmann
- * Sp
- *********************************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 package byui.CIT260.DaenirisTheForgotten.Control;
 
-
-
+/**
+ *
+ * @author Joshua
+ */
 public class SpellControl {
 
        public int weakenSpell(double specialEffect, int enemyDefense ){
