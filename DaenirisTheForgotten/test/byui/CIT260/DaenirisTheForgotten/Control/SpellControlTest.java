a29e6ac76e283da7d7e292e3cf7c6351d5669aa7
Nathan <Nathan@Wittmann-Laptop>
2014-05-28 21:27:53 -0600

    Individual Assignment #4  Fixed
    Conflicts:
    	DaenirisTheForgotten/test/byui/CIT260/DaenirisTheForgotten/Control/SpellControlTest.java

--- DaenirisTheForgotten/test/byui/CIT260/DaenirisTheForgotten/Control/SpellControlTest.java
+++ DaenirisTheForgotten/test/byui/CIT260/DaenirisTheForgotten/Control/SpellControlTest.java
@@ -1,8 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*********************************************************************
+ * SpellControlTest Class
+ * Author: Nathan Wittmann
+ * Test for weakenSpell
+ *********************************************************************/
 
 package byui.CIT260.DaenirisTheForgotten.Control;
 
@@ -11,26 +11,88 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Joshua
+ * @author Nathan
  */
 public class SpellControlTest {
     
     public SpellControlTest() {
     }
 
-    /**
-     * Test of weakenSpell method, of class SpellControl.
-     */
     @Test
+    
+    /************************************************************************
+     * Test #1
+     ************************************************************************/    
     public void testWeakenSpell() {
-        System.out.println("weakenSpell");
-        double specialEffect = 0.5;
-        int enemyDefense = 8;
+
+        System.out.println("weaken Spell Test #1");
+        double specialEffect = 0.8;
+        int enemyDefense = 10;
         SpellControl instance = new SpellControl();
-        int expResult = 4;
+        int expResult = 8;
         int result = instance.weakenSpell(specialEffect, enemyDefense);
         assertEquals(expResult, result);
         
+    /************************************************************************
+     * Test #2
+     ************************************************************************/        
+        System.out.println("weaken Spell Test #2");
+        specialEffect = 0.7;
+        enemyDefense = 10;
+        expResult = 7;
+        result = instance.weakenSpell(specialEffect, enemyDefense);
+        assertEquals(expResult, result);
+        
+    /************************************************************************
+     * Test #3
+     ************************************************************************/        
+        System.out.println("weaken Spell Test #3");
+        specialEffect = 0.6;
+        enemyDefense = 10;
+        expResult = 6;
+        result = instance.weakenSpell(specialEffect, enemyDefense);
+        assertEquals(expResult, result);    
+
+    /************************************************************************
+     * Test #4
+     ************************************************************************/        
+        System.out.println("weaken Spell Test #4");
+        specialEffect = -0.1;
+        enemyDefense = 10;
+        expResult = -1;
+        result = instance.weakenSpell(specialEffect, enemyDefense);
+        assertEquals(expResult, result);    
+
+    /************************************************************************
+     * Test #5
+     ************************************************************************/        
+        System.out.println("weaken Spell Test #5");
+        specialEffect = 0;
+        enemyDefense = 10;
+        expResult = -1;
+        result = instance.weakenSpell(specialEffect, enemyDefense);
+        assertEquals(expResult, result); 
+    
+    /************************************************************************
+     * Test #6
+     ************************************************************************/        
+        System.out.println("weaken Spell Test #6");
+        specialEffect = .9;
+        enemyDefense = 10;
+        expResult = 9;
+        result = instance.weakenSpell(specialEffect, enemyDefense);
+        assertEquals(expResult, result); 
+    
+    /************************************************************************
+     * Test #7
+     ************************************************************************/        
+        System.out.println("weaken Spell Test #7");
+        specialEffect = .5;
+        enemyDefense = 10;
+        expResult = 5;
+        result = instance.weakenSpell(specialEffect, enemyDefense);
+        assertEquals(expResult, result); 
+
     }
     
 }
