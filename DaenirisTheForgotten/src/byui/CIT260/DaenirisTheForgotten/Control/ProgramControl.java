302e82e064dd4ab791a9339f2975f92923bbb1b3
Joshua2112 <Joshua.lynn.larson@gmail.com>
2014-05-29 16:16:54 -0600

    view layer start

---
+++ DaenirisTheForgotten/src/byui/CIT260/DaenirisTheForgotten/Control/ProgramControl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.CIT260.DaenirisTheForgotten.Control;
+
+/**
+ *
+ * @author Joshua
+ */
+public class ProgramControl {
+
+    public static Player createPlayer(String playersName) {
+        System.out.println("createPlayer function was called");
+        return null;
+    }
+    
+    public class Player{
+        
+    }
+    
+}
---
+++ DaenirisTheForgotten/src/byui/CIT260/DaenirisTheForgotten/View/ProgramStart.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.CIT260.DaenirisTheForgotten.View;
+
+import byui.CIT260.DaenirisTheForgotten.Control.ProgramControl;
+import byui.CIT260.DaenirisTheForgotten.Control.ProgramControl.Player;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joshua
+ */
+public class ProgramStart{
+    
+    public void displayBanner(){
+        
+        System.out.println("this is the display message");
+    }
+    
+    public String getPlayersName(){
+        boolean valid = false;
+        String playersName = null;
+        Scanner keyboard = new Scanner(System.in);
+        
+        while(!valid){
+            
+            System.out.println("Please enter player name");
+            
+            playersName = keyboard.nextLine();
+            playersName = playersName.trim();
+            
+            if (playersName.toUpperCase().equals("Q")){
+                return null;
+            }
+            
+            if (playersName.length() < 2){
+                System.out.println("Invalid name - the name must be greater than one character in length");
+                
+            }
+            
+            else{
+                valid = true;
+            }
+        }
+        return playersName;
+    }        
+    
+    public void displayMainMenu(){
+        
+        System.out.println("/n/tEnter /'s/' to start new game"
+                + "         /n/tEnter /'l/' to load previously saved game"
+                + "         /n/tEnter /'q/' to quit to the desktop");
+    }
+    
+    public void startProgram(){
+            
+            this.displayBanner();
+            
+            String playersName = this.getPlayersName();
+            if (playersName == null)
+                return;
+            
+            Player player = ProgramControl.createPlayer(playersName);
+            
+            this.displayWelcomeMessage(player);
+            
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.displayMainMenu();
+            
+            }
+
+    private void displayWelcomeMessage(Player player) {
+        System.out.println("Welcome to the game" + player.getName());
+        
+    }
+    
+}
