745c72e09c483d0824236c6f2e1b8d2bd64ed889
Joshua2112 <Joshua.lynn.larson@gmail.com>
2014-06-06 15:39:11 -0600

    Team assignment 6

---
+++ DaenirisTheForgotten/src/byui/CIT260/DaenirisTheForgotten/View/UseItemMenuView.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.CIT260.DaenirisTheForgotten.View;
+
+/**
+ *
+ * @author Joshua
+ */
+class UseItemMenuView {
+
+    void displayItemMenu() {
+        System.out.println("use item stub called");
+    }
+    
+}
