1fb45967fee75fd2a11eb7fef614e294643470ed
Nathan <Nathan@Wittmann-Laptop>
2014-07-06 22:48:48 -0600

    Fixed exception
    Nathan Wittmann

--- DaenirisTheForgotten/src/byui/CIT260/DaenirisTheForgotten/View/EnterAgeView.java
+++ DaenirisTheForgotten/src/byui/CIT260/DaenirisTheForgotten/View/EnterAgeView.java
@@ -19,22 +19,37 @@ import java.util.Scanner;
  * @author Joshua
  */
 class EnterAgeView extends View{
-    
-    Actor[][] actors = game.getActor();
-    PlayerCharacter player = ((PlayerCharacter) actors[0][0]);
 
     public EnterAgeView(){
         super("\n\tPlease enter character's age (in numeric format[i.e. 18, 23, etc.])");
     }
 
     @Override
-    public int doAction(String value)throws illegalActionException{
-        try{
-        int age = Integer.parseInt(value);
-        player.setAge(age);
-        }catch(NumberFormatException nf){
-        throw new illegalActionException("\n\tMust be a number");
-        }
-        return 1;
+    public int doAction(String value){
+        int exit = 0;
+
+
+            try{
+                int age = checkAge(value);
+            }catch(illegalActionException ex){
+                System.out.println(ex.getMessage());
+                exit = 0;
             }
+
+        return exit;
     }
+    public static int checkAge(String value)throws illegalActionException{
+        Actor[][] actors = game.getActor();
+        PlayerCharacter player = ((PlayerCharacter) actors[0][0]);
+        int age = 0;
+        
+        try{        
+            age = Integer.parseInt(value);
+            player.setAge(age);
+        }catch(NumberFormatException ex){
+            throw new illegalActionException("Must enter a number");
+        }
+               
+        return age;
+    }
+}
