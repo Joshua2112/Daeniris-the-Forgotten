afdd37e24099700507fad7ecbba4ee92e266400d
Joshua2112 <Joshua.lynn.larson@gmail.com>
2014-07-22 21:37:26 -0600

    shop n stuff

--- DaenirisTheForgotten/src/byui/CIT260/DaenirisTheForgotten/Frames/AdventureMenuFrame.java
+++ DaenirisTheForgotten/src/byui/CIT260/DaenirisTheForgotten/Frames/AdventureMenuFrame.java
@@ -166,7 +166,6 @@ public class AdventureMenuFrame extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         mapTable = new javax.swing.JTable();
         jPanel3 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
         shopButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -460,13 +459,6 @@ public class AdventureMenuFrame extends javax.swing.JFrame {
         jScrollPane1.setViewportView(mapTable);
         mapTable.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
 
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
         shopButton.setText("Shop");
         shopButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -479,18 +471,14 @@ public class AdventureMenuFrame extends javax.swing.JFrame {
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(shopButton)
-                    .addComponent(jButton1))
+                .addGap(27, 27, 27)
+                .addComponent(shopButton)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addComponent(jButton1)
-                .addGap(18, 18, 18)
+                .addContainerGap()
                 .addComponent(shopButton)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -574,10 +562,6 @@ public class AdventureMenuFrame extends javax.swing.JFrame {
         this.mapTable.repaint();
     }//GEN-LAST:event_moveSouthActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        this.populateCharacterData();
-    }//GEN-LAST:event_jButton1ActionPerformed
-
     private void interactActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_interactActionPerformed
         boolean interact = BattleControl.createNewBattle(world);
         BattleFrame battle = new BattleFrame(this);
@@ -637,7 +621,6 @@ public class AdventureMenuFrame extends javax.swing.JFrame {
     private javax.swing.JLabel healtPoints;
     private javax.swing.JButton interact;
     private javax.swing.JButton itemsMenu;
-    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
